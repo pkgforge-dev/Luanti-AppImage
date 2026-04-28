f93861b175800ea9a83f56fbd89f341422eb559d
Link4Electronics <link4electronics@gmail.com>
2026-04-28 16:38:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,14 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm luanti sdl2 libldap
+pacman -Syu --noconfirm sdl2 libldap libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 
